5fc623248aae8c3c6420521d82bbfa1d4ffcdd7c
unknown <henriquejunqueira15@gmail.com>
2025-08-16 12:47:15 -0300

    Tipos básico de dados C# parte 2 e funções para valores mínimo e máximo

--- modulo03/TiposBasicosDados/TiposBasicosDados/Program.cs
+++ modulo03/TiposBasicosDados/TiposBasicosDados/Program.cs
@@ -4,6 +4,7 @@ namespace TiposBasicosDados {
     class Program {
         static void Main(string[] args) {
 
+            // Tipos básicos em C#
             // sbyte x = 100; // Tipo primitivo C#
             // SByte x = 100; // Tipo .Net Framework
 
@@ -13,16 +14,43 @@ namespace TiposBasicosDados {
             // byte n1 = 254;
             // n1++;
 
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
+            //bool completo = false;
+            //char genero = 'F';
+            //char letra = '\u0041';
+
+            //byte n1 = 126;
+            //int n2 = 1000;
+            //int n3 = 2147483647;
+            //long n4 = 2147483648L;
+            //float n5 = 4.5f; // A letra f indica que é um float
+            //double n6 = 4.5;
+            //string nome = "Maria Green"; // string é do tipo referência e é imutável
+            //object obj1 = "Alex Brown"; // object é do tipo genérico
+            //object obj2 = 4.2; // object é do tipo genérico
+
+            //Console.WriteLine(completo);
+            //Console.WriteLine(genero);
+            //Console.WriteLine(letra);
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+            //Console.WriteLine(n5);
+            //Console.WriteLine(n6);
+            //Console.WriteLine(nome);
+            //Console.WriteLine(obj1);
+            //Console.WriteLine(obj2);
+
+            // Funções de valores mínimos e máximos
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
 
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
-
         }
     }
 }
