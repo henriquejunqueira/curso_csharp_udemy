d1aef57bd4ddd6a2259d364ad680fee267f918e4
unknown <henriquejunqueira15@gmail.com>
2025-08-16 12:29:01 -0300

    Tipos básico de dados C# parte 1

---
+++ modulo03/TiposBasicosDados/TiposBasicosDados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TiposBasicosDados {
+    class Program {
+        static void Main(string[] args) {
+
+            // sbyte x = 100; // Tipo primitivo C#
+            // SByte x = 100; // Tipo .Net Framework
+
+            // Console.WriteLine(x);
+
+            // byte n1 = -1; // Não aceita número negativo
+            // byte n1 = 254;
+            // n1++;
+
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+
+        }
+    }
+}
