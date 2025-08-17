6586cbe36f16b8f58f76ef065d7af48909d42de9
unknown <henriquejunqueira15@gmail.com>
2025-08-17 15:51:20 -0300

    Operadores de Atribuição

---
+++ modulo03/OperadoresAtribuicao/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperadoresAtribuicao {
+    class Program {
+        static void Main(string[] args) {
+            //int a = 10;
+
+            //Console.WriteLine(a);
+
+            //a += 2;
+            //Console.WriteLine(a);
+
+            //a *= 3;
+            //Console.WriteLine(a);
+
+            //string s = "ABC";
+            //Console.WriteLine(s);
+
+            //s += "DEF";
+
+            //int a = 10;
+            //a++;
+            //Console.WriteLine(a);
+
+            //int a = 10;
+            //a--;
+            //Console.WriteLine(a);
+
+            //int a = 10;
+            //int b = a++;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            int a = 10;
+            int b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
