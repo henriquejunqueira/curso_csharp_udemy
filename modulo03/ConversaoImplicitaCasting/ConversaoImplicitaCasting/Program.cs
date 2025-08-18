2034391ed188209051c50d1d31b5af090637035e
unknown <henriquejunqueira15@gmail.com>
2025-08-17 21:06:56 -0300

    Conversão implícita e casting

---
+++ modulo03/ConversaoImplicitaCasting/ConversaoImplicitaCasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConversaoImplicitaCasting {
+    class Program {
+        static void Main(string[] args) {
+
+            //float x = 4.5f;
+
+            //double y = x; // Conversão implícita
+
+            //Console.WriteLine(y);
+
+            //double a;
+            //float b;
+            //int b;
+
+            //a = 5.1;
+            //b = (float) a; // Casting
+            //b = (int) a; // Casting
+
+            //Console.WriteLine(b);
+
+            int a = 5;
+            int b = 2;
+
+            double resultado = a / b;
+
+            Console.WriteLine(resultado );
+
+        }
+    }
+}
